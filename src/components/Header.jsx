a2810d14dbe10540fc72a2567f69a9f9e2c667ed
kenaneroz <kenanerozofficial@gmail.com>
2025-08-02 11:58:08 +0300

    Changes made

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { LuSearch } from "react-icons/lu";
 
 export default function Header(props) {
     return (
-        <header className="bg-[url(/business.png)] bg-cover bg-center pb-[125px]">
+        <header className="bg-[url(/business.jpg)] bg-cover bg-center pb-[125px]">
             <div className="bg-white flex justify-between items-center px-[25px] py-[10px]">
                 <select name="languages" id="languages" className="appearance-none cursor-pointer bg-[#50110A] text-white h-max rounded-full p-[2px]" onChange={(e) => props.setSelectedLanguage(e.target.value)}>
                     <option value="English">English</option>
