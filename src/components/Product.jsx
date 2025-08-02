e6d336a7487433ff55f8de32e7557ee6eaac562c
kenaneroz <kenanerozofficial@gmail.com>
2025-08-02 12:16:25 +0300

    Changes made

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,15 +2,13 @@ export default function Product(props) {
     return (
         <div 
             style={{ backgroundImage: `url(${props.imgUrl})` }}
-            className="w-full min-w-0 flex-shrink-0 max-w-full aspect-square bg-no-repeat bg-center bg-cover cursor-pointer relative rounded-[25px] overflow-hidden"
+            className="w-full aspect-square bg-no-repeat bg-center bg-cover cursor-pointer relative rounded-[25px] overflow-hidden"
             onClick={props.onClick}
         >
-            {/* Overlay */}
             <div className="absolute inset-0 bg-[#50110A]/75 z-10"></div>
 
-            {/* Centered content */}
             <div className="absolute inset-0 z-20 flex justify-center items-center">
-                <p className="text-white text-center text-base leading-tight px-2">
+                <p className="text-white text-center px-[10px]">
                     {props.title}
                 </p>
             </div>
