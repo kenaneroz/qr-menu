9ddb48014de331bb53290b1e8b9b3e70cac0093e
kenaneroz <kenanerozofficial@gmail.com>
2025-08-02 12:10:34 +0300

    Changes made

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,11 +2,11 @@ export default function Product(props) {
     return (
         <div 
             style={{backgroundImage: `url(${props.imgUrl})`}}
-            className="w-full min-w-0 flex-shrink-0 aspect-square bg-no-repeat bg-center bg-cover cursor-pointer relative rounded-[25px] p-[10px]"
+            className="w-full min-w-0 flex-shrink-0 aspect-square bg-no-repeat bg-center bg-cover cursor-pointer relative flex justify-center items-center rounded-[25px] p-[10px]"
             onClick={props.onClick}
         >
             <div className="absolute inset-0 bg-[#50110A]/75 rounded-[25px]"></div>
-            <div className="relative flex justify-center items-center h-full">
+            <div className="relative z-10 flex justify-center items-center h-full">
                 <p className="text-white text-center">{props.title}</p>
             </div>
         </div>
