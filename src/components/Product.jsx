53bd7276261c532cf8bda6a460302342721b5f73
kenaneroz <kenanerozofficial@gmail.com>
2025-08-02 12:19:01 +0300

    Changes made

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ export default function Product(props) {
     return (
         <div 
             style={{ backgroundImage: `url(${props.imgUrl})` }}
-            className="w-full aspect-square bg-no-repeat bg-center bg-cover cursor-pointer relative rounded-[25px] overflow-hidden px-[10px]"
+            className="aspect-square bg-no-repeat bg-center bg-cover cursor-pointer relative rounded-[25px] overflow-hidden px-[10px]"
             onClick={props.onClick}
         >
             <div className="absolute inset-0 bg-[#50110A]/75 z-10"></div>
